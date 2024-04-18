ae60d4128a7dcffc5911512f470b59a7cff9bbe6
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-04-18 13:17:39 +0200

    risolto esercizio cinque (il quattro era uguale al tre)

--- LezioneDue/lezioneDue.py
+++ LezioneDue/lezioneDue.py
@@ -31,4 +31,12 @@ quote: str = "Everyone is a Ferrari fan. Even if they say they're not, they are 
 print(f"{famous_person} once said, \"{quote}\"")
 print("-------------------------------")
 
+"""2-8. File Extensions: Python has a removesuffix() method that works exactly like removeprefix(). 
+Assign the value 'python_notes.txt' to a variable called filename. 
+Then use the removesuffix() method to display the filename without the file extension, like some file browsers do.
+"""
+filename: str = "python_notes.txt"
+filename: str = filename.removesuffix(".txt")
+print(filename)
+
 

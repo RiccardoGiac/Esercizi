258dd97c697365809c979fc40721450336ba8ea3
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-04-18 12:24:21 +0200

    risolto primo esercizio

---
+++ LezioneDue/lezioneDue.py
@@ -0,0 +1,12 @@
+#Riccardo Giacalone 18/04/24
+
+print("hello world")
+
+"""
+2-3. Personal Message: Use a variable to represent a person’s name, and print a message 
+to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+"""
+
+name: str = "Riccardo"
+message: str = f"Hello {name}, would you like to learn some Python today?"
+print(message)

d7ecd605c56a413263e22016b0fbaab3f40a25dd
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-04-18 13:05:16 +0200

    risolto esercizio tre

--- LezioneDue/lezioneDue.py
+++ LezioneDue/lezioneDue.py
@@ -20,3 +20,15 @@ name_upper: str = name.upper()
 name_lower: str = name.lower()
 print(f"{name}, {name_upper}, {name_lower}")
 print("-------------------------------")
+
+"""
+2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+"""
+famous_person: str = "Sebastian Vettel"
+quote: str = "Everyone is a Ferrari fan. Even if they say they're not, they are Ferrari fans."
+print(f"{famous_person} once said, \"{quote}\"")
+print("-------------------------------")
+
+

fd2f1d931d07c3d1975e98319380b69ba6a1e767
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-04-19 12:53:10 +0200

    Svolto secondo esercizio sulle funzioni

--- LezioneQuattro/lezioneQuattro.py
+++ LezioneQuattro/lezioneQuattro.py
@@ -5,3 +5,16 @@ def subtract(x: int, y: int) -> int:
     return x - y
 
 print(subtract(6,4))
+print("-------------------------------------")
+
+#Esercizio due: numero maggiore, minore o uguale a 5
+
+def check_value(x: int) -> int:
+    if x > 5:
+        print(f"Il numero {x} è maggiore di 5")
+    elif x == 5:
+        print(f"Il numero {x} è uguale a 5")
+    else:
+        print(f"Il numero {x} è minore di 5")
+
+check_value(4)

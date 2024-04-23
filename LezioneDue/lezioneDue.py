900bec10024e3512f3e934010f42260d9ed62ad1
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-04-23 19:37:36 +0200

    Iniziato esercizio 6-9.

--- LezioneDue/lezioneDue.py
+++ LezioneDue/lezioneDue.py
@@ -293,5 +293,12 @@ for i in range(len(pets)):
         print(f"{key} : {pets[i][key]}")
 print("-------------------------------")
 
+"""
+6-9. Favorite Places: Make a dictionary called favorite_places. 
+Think of three names to use as keys in the dictionary, and store one to three favorite places for each person.
+To make this exercise a bit more interesting, ask some friends to name a few of their favorite places. 
+Loop through the dictionary, and print each person’s name and their favorite places.
+"""
+favorite_places: dict = {"Fabrizio" : "Spain,France,Greece", "Andrea" : "Japan,Thailand,South Korea", "Luigi" : "America, Iceland"}
 
 

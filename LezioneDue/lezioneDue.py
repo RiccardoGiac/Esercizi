a8f5fc3fce3cb8a7bc6dfb5613baab2eb89bcd6b
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-04-23 18:18:23 +0200

    Svolto esercizio 6-1.

--- LezioneDue/lezioneDue.py
+++ LezioneDue/lezioneDue.py
@@ -230,6 +230,17 @@ food.remove("Pizza")
 print(food)
 print("We don't need this list anymore")
 del(food)
+print("-------------------------------")
+
+"""
+6-1. Person: Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live.
+ You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.
+"""
+person: dict = {"first_name" : "Fabrizio", "last_name" : "Barberi", "age" : 29, "city" : "Aprilia" }
+print(f"{person['first_name']} {person['last_name']} of age {person['age']} lives in {person['city']} ")
+print("-------------------------------")
+
+
 
 
 

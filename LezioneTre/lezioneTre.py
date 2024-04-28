687f0afc1b5fd1d00df52d0fe8bc25406da7c0cc
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-04-28 16:04:24 +0200

    Svolto 4-11.

--- LezioneTre/lezioneTre.py
+++ LezioneTre/lezioneTre.py
@@ -1,4 +1,5 @@
 #Riccardo Giacalone
+import copy
 """
 4-1. Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a list, and then use a for loop to print the name of each pizza.
 • Modify your for loop to print a sentence using the name of the pizza, instead of printing just the name of the pizza. 
@@ -101,5 +102,23 @@ print(f"The last three items in odds list are: {(odds[-3::])}")
 print("-------------------------------------")
 
 """
+4-11. My Pizzas, Your Pizzas: Start with your program from Exercise 4-1. Make a copy of the list of pizzas, and call it friend_pizzas. Then, do the following:
+• Add a new pizza to the original list.
+• Add a different pizza to the list friend_pizzas.
+• Prove that you have two separate lists. Print the message My favorite pizzas are:, and then use a for loop to print the first list. 
+Print the message My friend’s favorite pizzas are:, and then use a for loop to print the second list. Make sure each new pizza is stored in the appropriate list.
+"""
+friend_pizzas:list = copy.copy(pizzas)
+pizzas.append("Marinara")
+friend_pizzas.append("Capricciosa")
+for i in pizzas:
+    print(f"My favorite pizzas are: {pizzas}")
+for i in friend_pizzas:
+    print(f"My friends favorite pizzas are: {friend_pizzas}")
+print(f"{pizzas} \n{friend_pizzas}")
+print("-------------------------------------")
 
+"""
+4-12. More Loops: All versions of foods.py in this section have avoided using for loops when printing, to save space. 
+Choose a version of foods.py, and write two for loops to print each list of foods.
 """

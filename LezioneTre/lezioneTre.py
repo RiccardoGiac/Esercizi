428c1e0a44b86e68add55a2a9d901a01599c9e43
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-04-28 15:50:51 +0200

    Svolto esercizio 4-10

--- LezioneTre/lezioneTre.py
+++ LezioneTre/lezioneTre.py
@@ -79,7 +79,7 @@ print("-------------------------------------")
 4-8. Cubes: A number raised to the third power is called a cube. For example, the cube of 2 is written as 2**3 in Python. 
 Make a list of the first 10 cubes (that is, the cube of each integer from 1 through 10), and use a for loop to print out the value of each cube.
 """
-cubes: list = [i**3 for i in range(1, 11)]
+cubes: list = [i**3 for i in range(1, 11)]                  #Per ogni elemento tra 1 e 11(da 1 a 10) fai elemento al cubo(i**3) e mettilo nella lista cubes
 for i in cubes:
     print(i)
 print("-------------------------------------")
@@ -92,4 +92,14 @@ print("-------------------------------------")
 • Print the message The first three items in the list are:. Then use a slice to print the first three items from that program’s list.
 • Print the message Three items from the middle of the list are:. Then use a slice to print three items from the middle of the list.
 • Print the message The last three items in the list are:. Then use a slice to print the last three items in the list.
+"""
+#userò la lista odds:
+print(f"The first three items in odds list are: {(odds[0:3])}")
+mid: int = len(odds)//2 - 1
+print(f"The three middle items in odds list are: {odds[mid:mid+3]}")
+print(f"The last three items in odds list are: {(odds[-3::])}")
+print("-------------------------------------")
+
+"""
+
 """

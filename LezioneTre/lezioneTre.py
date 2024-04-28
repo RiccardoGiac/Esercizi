80008e142a816714c332a80931afd78b252ad70c
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-04-29 00:07:43 +0200

    Svolti esercizi 5-3 e 5-4

--- LezioneTre/lezioneTre.py
+++ LezioneTre/lezioneTre.py
@@ -246,3 +246,33 @@ if 5 in ls:
 else:
     print("5 is not in the list")
 print("-------------------------------------")
+"""
+5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+"""
+alien_color: str = "green"
+if alien_color == "green":
+    print("+5 points")
+if alien_color == "yellow":
+    print("+3 points")
+if alien_color == "red":
+     None
+print("-------------------------------------")
+
+"""
+5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+• Write one version of this program that runs the if block and another that runs the else block.
+"""
+if alien_color == "green":
+    print("+5 points")
+else:
+    print("+10 points")
+if alien_color != "green":
+    print("+5 points")
+else:
+    print("+10 points")
+print("-------------------------------------")
+

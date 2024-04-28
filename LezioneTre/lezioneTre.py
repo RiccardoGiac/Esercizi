3f06e1d327f75c3078e2ce23d10b7464eb8785b5
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-04-28 15:14:06 +0200

    Risolti esercizi 4-5 e 4-6

--- LezioneTre/lezioneTre.py
+++ LezioneTre/lezioneTre.py
@@ -46,6 +46,23 @@ print("-------------------------------------")
 (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
 """
 millions: list = [i for i in range(1, 1000001)]
-for i in millions:
+#for i in millions:
+#    print(i)
+print("-------------------------------------")
+
+"""
+4-5. Summing a Million: Make a list of the numbers from one to one million, and then use min() and max() to make sure your list actually starts at one and ends at one million. 
+Also, use the sum() function to see how quickly Python can add a million numbers.
+"""
+print(min(millions))
+print(max(millions))
+print(sum(millions))
+print("-------------------------------------")
+
+"""
+4-6. Odd Numbers: Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. Use a for loop to print each number.
+"""
+odds: list = [i for i in range(1, 20, 2)]
+for i in odds:
     print(i)
 print("-------------------------------------")

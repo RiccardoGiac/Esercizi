a2b1ab338df7e78f431f4ed98c0abeabc5a4a0f5
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-05-12 19:17:14 +0200

    aggiunto a Class Fence la list[Animal] che ci sono dentro e definito add e remove animal

--- ProgettoZoo/zoo.py
+++ ProgettoZoo/zoo.py
@@ -9,6 +9,7 @@ class Animal:
         self.width = width
         self.preferred_habitat = preferred_habitat
         self.health = round(100*(1/age),3)
+        self.area = round(width * height,3)
     
 class Fence:
 
@@ -16,6 +17,7 @@ class Fence:
         self.area = area
         self.temperature = temperature
         self.habitat = habitat
+        self.animals_inside : list[Animal] = []
 
 class Zookeeper:
 
@@ -25,10 +27,19 @@ class Zookeeper:
         self.id = id 
 
     def add_animal(self,animal:Animal,fence:Fence):
-        pass
+        if fence.area - animal.area >= 0 and animal.preferred_habitat == fence.habitat:
+            fence.area -= animal.area
+            fence.animals_inside.append(animal)
+        else:
+            print(f"Cannot add {animal.name} in this fence.")
 
     def remove_animal(self,animal:Animal,fence:Fence):
-        pass
+        if animal in fence.animals_inside:
+            fence.animals_inside.remove(animal)
+            fence.area += animal.area
+        else:
+            print("Animal is not in this fence")
+
 
     def feed(self,animal:Animal):
         pass
@@ -44,3 +55,11 @@ class Zoo:
 
     def describe_zoo(self):
         pass
+
+    ######## TESTS #############
+    a1 : Animal = Animal("pippo", "lupo",5, 23.3, 10, "Mountain")
+    f1 : Fence = Fence(1000,20,"Mountain")
+    zk1: Zookeeper = Zookeeper("Gino", "Baglio", "135G")
+    zk1.add_animal(a1,f1)
+    f2: Fence = Fence(2000,30,"Savana")
+    zk1.remove_animal(a1,f2)

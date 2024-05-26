dcd86081920a74f9ffd889a2f39f7f5e25b37cea
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-05-26 16:04:36 +0200

    aggiunti file degli es. sudoku is_symmetric e lista_concatenata

--- test.py
+++ test.py
@@ -1,23 +1,19 @@
-def word_break(s:str, wordDict:list[str]) -> bool:
-
-    sl: list[str] = list(s)
-    l_compare: list[str] = []
-    l_compare2: list[str] = []
-    s_attuale: str = sl[0]
-    for letter in sl:
-        if s_attuale == wordDict[0]:
-            l_compare.append(wordDict)
-            l_compare2.append(wordDict.pop(0))
-            s_attuale = sl[0]
-        else:
-            s_attuale += letter
-                        
-    if l_compare == l_compare2:
-            return True
-    else:
-            return False
-
+def reverse(x: int) -> int:
+        sxs: str = str(x)
+        sx : list = list(sxs)
+        if sx[0] == "-":
+            sx.remove(sx[0])
+        for n in sx:
+            sx.append(sx.pop(0))
         
-                
-
-print(word_break("catsandog",["cats","dog","sand","and","cat"]))
+        sx = str(sx)
+        print(sx)
+        
+        if x < 0:
+            sx = int(sx)
+            sx = -(sx)
+            return sx
+        else:
+            return int(sx)
+        
+print(reverse(-2))

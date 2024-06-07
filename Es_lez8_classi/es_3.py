e6512d56ed694e692a3d965c37778b4e97980031
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-06-07 08:56:14 +0200

    erergdg

--- Es_lez8_classi/es_3.py
+++ Es_lez8_classi/es_3.py
@@ -17,14 +17,42 @@ class Book:
         
 class Member:
 
-    member_id:int = 0
+    m_id:int = 0
 
-    def __init__(self, name: str, member_id: int) -> None:
+    def __init__(self, name: str) -> None:
         self.name: str = name
-        self.member_id: int = member_id + 1
+        self.member_id: int = Member.m_id
         self.borrowed_books: list[Book] = []
+        Member.m_id += 1
 
     def borrow_book(self,book: Book):
         if book not in self.borrowed_books:
             self.borrowed_books.append(book)
+        else:
+            print("Book already borrowed")
 
+    def return_book(self,book: Book):
+        if book in self.borrowed_books:
+            self.borrowed_books.remove(book)
+        else:
+            print("Book isn't borrowed")
+
+    def __str__(self) -> str:
+        f"Member = {self.name}, {self.member_id}, {self.borrowed_books}"
+
+    @classmethod
+    def from_string(cls,member_str :str):
+        return Member(member_str)
+    
+
+class Library:
+
+    total_books: int = 0
+
+    def __init__(self, books: list[Book] = [], members: list[Member]=[]) -> None:
+        self.books: list[Book] = books
+        self.members: list[Member] = members
+    
+    def add_book(self, book: Book):
+        Library.total_books += 1
+    

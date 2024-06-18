4c29dc3ae0f87480e49a24cdf97269bac9a6397a
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-06-18 13:55:40 +0200

    in bubble, fatto anche il  merge

--- LezioneDiciassette/SistemaOspedaliero/test_persona.py
+++ LezioneDiciassette/SistemaOspedaliero/test_persona.py
@@ -30,6 +30,29 @@ class TestPersona(unittest.TestCase):
         self.assertEqual(self.p1.getAge(),20,"Errore nel cambio eta")
 
 
+class TestDottore(unittest.TestCase):
+
+    def setUp(self) -> None:
+
+        self.d1: Dottore = Dottore(first_name="Giorgio",last_name="Giovanni",
+                                   specialization="QuelloChePassaIlBisturi",parcel=150.00)
+    
+    def test_attributi(self):
+
+        self.assertEqual(self.d1.getName(),"Giorgio","Errore nel nome del Dottore")
+        self.assertEqual(self.d1.getLastname(),"Giovanni","Errore nel cognome del Dottore")
+        self.assertEqual(self.d1.getAge(),31,"Errore nell'eta del Dottore")
+        
+    def test_validDoctor(self):
+
+        self.d1.age = 31
+        self.assertEqual(self.d1.isAValidDoctor(),print(f"Doctor {self.d1.first_name} {self.d1.last_name} is valid!"),"Errore nella validità del dottore")
+        self.d1.age = 20
+        self.assertEqual(self.d1.isAValidDoctor(),print(f"Doctor {self.d1.first_name} {self.d1.last_name} is not valid!"),"Errore nella validità del dottore")
+
+
+
+
 
 
 if __name__ == "__main__":

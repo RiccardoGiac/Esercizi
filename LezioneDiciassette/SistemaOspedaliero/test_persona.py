de6fe5bccd7687e80587b4e5107967b04fa450c0
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-06-20 21:37:02 +0200

    finito test sistema ospedaliero

--- LezioneDiciassette/SistemaOspedaliero/test_persona.py
+++ LezioneDiciassette/SistemaOspedaliero/test_persona.py
@@ -35,23 +35,72 @@ class TestDottore(unittest.TestCase):
     def setUp(self) -> None:
 
         self.d1: Dottore = Dottore(first_name="Giorgio",last_name="Giovanni",
-                                   specialization="QuelloChePassaIlBisturi",parcel=150.00)
+                                   specialization="QuelloChePassaIlBisturi",parcel=150.0)
     
     def test_attributi(self):
 
         self.assertEqual(self.d1.getName(),"Giorgio","Errore nel nome del Dottore")
         self.assertEqual(self.d1.getLastname(),"Giovanni","Errore nel cognome del Dottore")
         self.assertEqual(self.d1.getAge(),31,"Errore nell'eta del Dottore")
-        
+        self.assertEqual(self.d1.getParcel(), 150.0,"Errore nella parcella del dottore")
+    
     def test_validDoctor(self):
 
         self.d1.age = 31
-        self.assertEqual(self.d1.isAValidDoctor(),print(f"Doctor {self.d1.first_name} {self.d1.last_name} is valid!"),"Errore nella validità del dottore")
+        self.assertEqual(self.d1.isAValidDoctor())
         self.d1.age = 20
-        self.assertEqual(self.d1.isAValidDoctor(),print(f"Doctor {self.d1.first_name} {self.d1.last_name} is not valid!"),"Errore nella validità del dottore")
+        self.assertEqual(self.d1.isAValidDoctor())
 
 
+class TestPaziente(unittest.TestCase):
 
+    def setUp(self) -> None:
+
+        self.pa1: Paziente = Paziente(first_name="Carlo",last_name="Carlini",
+                                      id_code="24A")
+
+    def test_attributi(self):
+        
+        self.pa1.age = 20
+        self.assertEqual(self.pa1.getName(),"Carlo","Errore nel nome del Dottore")
+        self.assertEqual(self.pa1.getLastname(),"Carlini","Errore nel cognome del Dottore")
+        self.assertEqual(self.pa1.getAge(),20)
+        self.assertEqual(self.pa1.getIdCode(),"24A")   
+
+class TestFattura(unittest.TestCase):
+
+    def setUp(self) -> None:
+        
+        self.d: Dottore = Dottore("Giorgio","Rossi","Cardiologo",150.0)
+        self.d.setAge(55)
+        self.p: list[Paziente] = [Paziente(first_name="Giovanni",
+                                                  last_name="Verdi",
+                                                  id_code="33B",),
+                                         Paziente(first_name="Rosaria",
+                                                  last_name="Franchi",
+                                                  id_code="22A")]
+        self.f: Fattura = Fattura(self.p,self.d)
+
+    def test_attributi(self):
+        
+        self.assertEqual(self.f.fatture,2)
+        self.assertEqual(self.f.getSalary(),300.0)
+
+    def test_add_patient(self):
+
+        paziente: Paziente = Paziente("Luca","Nervi","LL4")
+        self.f.addPatient(paziente)
+        self.assertEqual(self.f.getFatture(),3)
+        self.assertEqual(self.f.getSalary(),450.0)
+
+    def test_remove_patient(self):
+
+        self.f.removePatient("22A")
+        self.assertEqual(self.f.getFatture(),1)
+        self.assertEqual(self.f.getSalary(),150.0)
+        
+                                                        
+        
 
 
 

08e3f4104eb7d9552e74e8fef0f43457b99bf5f1
RiccardoGiac <riccardo.giacalone.164@gmail.com>
2024-07-05 00:15:39 +0200

    pagamento

--- LezioneVenti/GestionalePagamento/pagamento.py
+++ LezioneVenti/GestionalePagamento/pagamento.py
@@ -32,23 +32,26 @@ class PagamentoContanti(Pagamento):
 
         for e in self.banconote:
             count: int = 0
-            while self.importo > e:
-                self.importo - e
+            while self.importo >= e:
+                self.importo = self.importo - e
                 count += 1
             d_soldi[e] = count
 
         for e in self.monete:
             count: int = 0
-            while self.importo > e:
-                self.importo - e
+            while self.importo >= e:
+                self.importo = self.importo - e
                 count += 1
             d_soldi[e] = count    
 
         for k in d_soldi:
-            if k >= 5:
-                print(f"{d_soldi[k]} banconota/e da {k} euro")
+
+            if d_soldi[k] == 0:
+                continue
+            elif k >= 5:
+                print(f"{d_soldi[k]} banconota/e da {k:.2f} euro")
             else:
-                print(f"{d_soldi[k]} moneta/e da {k} euro")
+                print(f"{d_soldi[k]} moneta/e da {k:.2f} euro")
 
                 
 class PagamentoCartaDiCredito(Pagamento):
@@ -60,6 +63,19 @@ class PagamentoCartaDiCredito(Pagamento):
         self.data_scad = data_scad
         self.n_carta = n_carta
 
+    def dettagli_pagamento(self):
+        return (f"Pagamento di: €{self.importo} effettuato con la carta di credito\n"
+                f"Nome sulla carta: {self.nome_cognome}\n"
+                f"Data di scadenza: {self.data_scad}\n"
+                f"Numero della carta: {self.n_carta}")
 
 
-        
+##TESTS##
+
+pc:PagamentoContanti=PagamentoContanti(1352.02)
+pc.inPezziDa()
+
+pcdc:PagamentoCartaDiCredito=PagamentoCartaDiCredito(44.23,"Riccardo",
+                                                     "23/12/25","4245235324523")
+
+print(pcdc.dettagli_pagamento())
